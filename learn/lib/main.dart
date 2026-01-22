b31cbd4bd2e3ae4716ee879cd54cb793b4229bdf
akshaygohel39004 <akshay.gohel@pirimidtech.com>
2026-01-22 17:11:26 +0530

    added image widget.

--- learn/lib/main.dart
+++ learn/lib/main.dart
@@ -2,17 +2,28 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,
         title: Text("Hello from my app"),
         centerTitle: true,
       ),
       body: Center(
-        child: Text("my application in flutter"),
+        child: Image(
+            image:NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJo7n7XXByw40QwFnGILGMq2BxD55PkKl8yA&s")),
       ),
       floatingActionButton: FloatingActionButton(onPressed: ()=>{
         print("clicked")
       },child: Text("click me"),),
-    ),
-  ));
+    );
+  }
 }
