a240b8d27dbe72a1e33508cb3c346cda78d4b6d5
akshaygohel39004 <akshay.gohel@pirimidtech.com>
2026-01-23 14:36:18 +0530

    demo of navigation in flutter.

--- learn/lib/main.dart
+++ learn/lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:learn/pages/choose_location.dart';
 import 'package:learn/pages/home.dart';
+import 'package:learn/pages/loading.dart';
 
 void main() => runApp(
   MaterialApp(
-    home:SafeArea(child: Home()),
+    initialRoute: '/home',
+    routes: {
+      '/':(context) => Loading(),
+      '/home':(context)=>Home(),
+      '/location': (context)=> ChooseLocation()
+    },
+    // home:SafeArea(child: Home()),
+
     // debugShowCheckedModeBanner: false,
   )
 );
